b047be244bb425a088cfd77465ee3d8780205dcb
Alistair Blake <consultations@me.com>
2018-11-05 22:17:37 -0800

    updates made

--- app.js
+++ app.js
@@ -7,21 +7,22 @@ var totalClicks = 0;
 var firstImg = document.getElementById('first');
 var secondImg = document.getElementById('second');
 var thirdImg = document.getElementById('third');
-//set up a constructor to setup an object for every image downloaded
-//and to keep track of the times the image is displayed.
-//instantiate new objects:
+//set the foundation to enable a constructor to setup an object for every image downloaded
+//and to keep track of the times the image is displayed, and then instantiate the new objects:
 var allProducts = [];
+//Constructor function for 'Product' objects:
 function Product(name, imgPath, altText) {
-  //image a property on the constructor function(product):
-
+  //(image a property on the constructor function(product))
   this.name = name;
   this.imgPath = imgPath;
   this.altText = altText;
   this.views = 0; //the other properties havent been seen.set them to 0
-  this.votes = 0; // everytime you click on an object increase this value
-  allProducts.push(this);//push this whenever the object is instantiated
-}
-//new product instantiates a new object
+  this.votes = 0; //everytime you click on an object increase this value
+  allProducts.push(this); //push this whenever the object is instantiated
+};
+//new product instantiates (to represent or be an example of something) a new object
+//we need a "blueprint" for creating many objects of the same "type".
+//The way to create an "object type", is to use an object constructor function:
 new Product('bag', './assets/bag.jpg');
 new Product('banana', './assets/banana.jpg');
 new Product('bathroom', './assets/bathroom.jpg');
@@ -50,7 +51,7 @@ function randomImage() {
   var secondRandom = Math.floor(Math.random() * allProducts.length);
   var thirdRandom = Math.floor(Math.random() * allProducts.length);
 
-  //grab a random image from the array; access it at the index 'randomFirst' on the array.
+  //grab a random image from the array; access it at the index 'randomFirst' on the array:
   firstImg.src = allProducts[firstRandom].imgPath;
   secondImg.src = allProducts[secondRandom].imgPath;
   thirdImg.src = allProducts[thirdRandom].imgPath;
@@ -69,14 +70,14 @@ function randomImage() {
 randomImage();
 //generate a string for every object
 function displayResults() {
-  //use a for loop to iterate through the array
-  for (var i = 0; i < allProducts.length; i++) {//start at 0. is 0 < 20. if yes increment it by 1 
+  //use a for loop to iterate through the array:
+  for (var i = 0; i < allProducts.length; i++) { //start at 0. is 0 < 20. if yes, increment it by 1 
     var listEl = document.createElement('li');
     listEl.textContent = allProducts[i].votes + ' votes for the ' + allProducts[i].name + ' and ' + allProducts[i].views + ' views ';
     results.appendChild(listEl);
   }
-}
-// add event listeners to receive the value of the callback function 
+};
+//add event listeners to receive the value of the callback function 
 firstImg.addEventListener('click', randomImage);
 secondImg.addEventListener('click', randomImage);
 thirdImg.addEventListener('click', randomImage);
